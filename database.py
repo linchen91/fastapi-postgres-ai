e6f0333fe93badf525b152171b6b1b3f5947aec4
Lin Chen <linchen91@hotmail.com>
2026-08-23 16:49:21 +0200

    Add database session dependency for route handlers

--- database.py
+++ database.py
@@ -14,3 +14,11 @@ DATABASE_URL = f'mysql+pymysql://{username}:{password}@{host}:{port}/{db}'
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
